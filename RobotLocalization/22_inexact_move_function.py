f4a1960acdeed391129b976b8218b011de1494d7
Саша Златковић <sashsoft@gmail.com>
2017-12-14 23:55:09 +0100

    konacna 22_inexact

--- RobotLocalization/22_inexact_move_function.py
+++ RobotLocalization/22_inexact_move_function.py
@@ -1,13 +1,13 @@
 #Modify the move function to accommodate the added
 #probabilities of overshooting or undershooting
 #the intended destination.
-p = [0,1,0,0,0]
+p = [1,0,0,0,0]
 world = ['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
-pExact = 0.8
-pOvershoot = 0.1
+pExact = 0.7
+pOvershoot = 0.2
 pUndershoot = 0.1
 
 def sense(p, Z):
@@ -21,24 +21,22 @@ def sense(p, Z):
     return q
 
 def move(p, U):
-    #for i in range(len(p)):
-    #    q.append(p[(i - U) % len(p)])
-
-    U = U % len(p)
-    #q = p[-U:] + p[:-U]
-    q = [0] * len(p)
-    for i in range(len(p) - U):
-        q[i] += p[-U + i] * pExact
-        q[i + 1] += p[-U + i] * pOvershoot
-        q[i - 1] += p[-U + i] * pUndershoot
-    #i = U = U % len(p)
-    #while i > 0:
-    #    q = q[-1:] + q[:-1]
-    #    i -= 1
-    #for i in q:
-    #    i = i
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i - U) % len(p)]
+        s = s + pOvershoot * p[(i - U - 1) % len(p)]
+        s = s + pUndershoot * p[(i - U + 1) % len(p)]
+        q.append(s)
     return q
-    
 
-print(move(p, 1))
+
+def move_moja(p, U):    
+    q = [0] * len(p)
+    for i in range(len(p)):
+        q[i] += (p[(-U + i) % len(p)] * pExact + p[(-U + i - 1) % len(p)] * pOvershoot + p[(-U + i + 1) % len(p)] * pUndershoot)
+    return q
+
+
+print(move(p, 10000000))
+print(move_moja(p, 10000000))
 
