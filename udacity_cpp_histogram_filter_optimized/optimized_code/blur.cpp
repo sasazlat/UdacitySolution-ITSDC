0ef4d2465dc950ca6101df8081827809b9822288
Саша Златковић <sashsoft@gmail.com>
2018-01-06 20:56:07 +0100

    blur and zeros finshed

--- udacity_cpp_histogram_filter_optimized/optimized_code/blur.cpp
+++ udacity_cpp_histogram_filter_optimized/optimized_code/blur.cpp
@@ -1,9 +1,10 @@
-#include "headers/blur.h"
+#include "headers\blur.h"
+#include "headers\zeros.h"
 
 using namespace std;
 
 // OPTIMIZATION: Pass large variable by reference
-vector < vector <float> > blur(vector < vector < float> > grid, float blurring) {
+vector < vector <float> > blur(vector < vector < float> > &grid, float blurring) {
 
 	// OPTIMIZATION: window, DX and  DY variables have the 
 	// same value each time the function is run.
@@ -15,14 +16,13 @@ vector < vector <float> > blur(vector < vector < float> > grid, float blurring) 
 	// bracket syntax: vector<int> foo = {1, 2, 3, 4} 
 	// instead of calculating these vectors with for loops 
 	// and push back
-	vector < vector <float> > window;
 	vector < vector <float> > newGrid;
 	vector <float> row;
 	vector <float> newRow;
 
 	int height;
 	int width;
-	float center, corner, adjacent;
+	static float center, corner, adjacent;
 
 	height = grid.size();
 	width = grid[0].size();
@@ -31,72 +31,17 @@ vector < vector <float> > blur(vector < vector < float> > grid, float blurring) 
 	corner = blurring / 12.0;
 	adjacent = blurring / 6.0;
 
+
+	static vector < vector <float> > window = { {corner, adjacent, corner},
+												{adjacent, center, adjacent},
+												{corner, adjacent, corner}
+	};
+
 	int i, j;
 	float val;
 
-	for (i = 0; i < 3; i++) {
-		row.clear();
-		for (j = 0; j < 3; j++) {
-			switch (i) {
-			case 0:
-				switch (j) {
-				case 0:
-					val = corner;
-					break;
-
-				case 1:
-					val = adjacent;
-					break;
-
-				case 2:
-					val = corner;
-					break;
-				}
-				break;
-
-			case 1:
-				switch (j) {
-				case 0:
-					val = adjacent;
-					break;
-
-				case 1:
-					val = center;
-					break;
-
-				case 2:
-					val = adjacent;
-					break;
-				}
-				break;
-
-			case 2:
-				switch (j) {
-				case 0:
-					val = corner;
-					break;
-
-				case 1:
-					val = adjacent;
-					break;
-
-				case 2:
-					val = corner;
-					break;
-				}
-				break;
-			}
-			row.push_back(val);
-		}
-		window.push_back(row);
-	}
-
-
-	vector <int> DX;
-	vector <int> DY;
-
-	DX.push_back(-1); DX.push_back(0); DX.push_back(1);
-	DY.push_back(-1); DY.push_back(0); DY.push_back(1);
+	static vector <int> DX = { -1, 0, 1 };
+	static vector <int> DY = { -1, 0, 1 };
 
 	int dx;
 	int dy;
@@ -108,13 +53,7 @@ vector < vector <float> > blur(vector < vector < float> > grid, float blurring) 
 	float newVal;
 
 	// OPTIMIZATION: Use your improved zeros function
-	for (i = 0; i < height; i++) {
-		newRow.clear();
-		for (j = 0; j < width; j++) {
-			newRow.push_back(0.0);
-		}
-		newGrid.push_back(newRow);
-	}
+	newGrid = zeros(height, width);
 
 	for (i = 0; i < height; i++) {
 		for (j = 0; j < width; j++) {
--- udacity_cpp_histogram_filter_optimized/optimized_code/zeros.cpp
+++ udacity_cpp_histogram_filter_optimized/optimized_code/zeros.cpp
@@ -3,20 +3,13 @@
 using namespace std;
 
 vector < vector <float> > zeros(int height, int width) {
-	int i, j;
-  
+	  
 	// OPTIMIZATION: Reserve space in memory for vectors
-	vector < vector <float> > newGrid;
-	vector <float> newRow;
+	
+	vector < vector <float> > newGrid(height, vector<float>(width, 0.0));	
 
   	// OPTIMIZATION: nested for loop not needed
     // because every row in the matrix is exactly the same
-	for (i=0; i<height; i++) {
-		newRow.clear();
-		for (j=0; j<width; j++) {
-			newRow.push_back(0.0);
-		}
-		newGrid.push_back(newRow);
-	}
+	
 	return newGrid;
 }
